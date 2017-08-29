f28e4ad4468923d124d74d4aff69c637f82807cc
Joe Fearnley <joe.fearnley@gmail.com>
2017-08-29 20:36:08 +0000

    Adding twitter client config

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const cheerio = require('cheerio');
 const axios = require('axios');
+const twitter = require('twitter');
+const twitterClient = new twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
 const baseUrl = 'http://www.repostered.com';
 
 const getSeriesUrl = async () => {
   const res = await axios.get(`${baseUrl}/labels`);
   const $ = cheerio.load(res.data);
   const designers = $('.designer-info');
-  const randomNumber = Math.floor(Math.random() * 21);
+  const randomNumber = Math.floor(Math.random() * randomNumber.length);
   return designers.eq(randomNumber).find('a').attr('href');
 };
 
@@ -14,28 +21,35 @@ const getPosterUrl = async url => {
   const res = await axios.get(`${baseUrl}${url}`);
   const $ = cheerio.load(res.data);
   const posters = $('.poster-container');
-  return posters.eq(Math.floor(Math.random() * posters.length)).find('a').attr('href');
+  const randomNumber = Math.floor(Math.random() * posters.length);
+  return posters.eq(randomNumber).find('a').attr('href');
 };
 
 const getPoster = async url => {
   const res = await axios.get(`${baseUrl}${url}`);
   const $ = cheerio.load(res.data);
-  const imageUrl = $('#posterBig').attr('src');
-  const title = $('.page-title > a').text();
   const designer = $('#poster-image').next().find('a').first();
-  const designerUrl = `${baseUrl}${designer.attr('href')}`;
 
   return { 
     title,
-    imageUrl,
-    url,
+    imageUrl: $('#posterBig').attr('src'),
+    url: $('.page-title > a').text(),
     designer: designer.text(),
-    designerUrl
+    designerUrl: `${baseUrl}${designer.attr('href')}`
   };
 };
 
 function sendTweet(poster) {
-  console.log(poster);
+  const tweet = {
+    status: poster.title
+  };
+
+  twitterClient.post('statuses/update', tweet, (error, tweet, response) => {
+      if (error) {
+        console.log('Error sending tweet');
+        console.log(error);
+      }
+    });
 }
 
 getSeriesUrl()
