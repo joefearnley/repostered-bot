d2311a49cb0222ee5d0b287a15ce73d69143557e
Joe Fearnley <joe.fearnley@gmail.com>
2017-09-27 19:02:33 -0400

    Spacing things out a bit

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const cheerio = require('cheerio');
 const axios = require('axios');
 const twitter = require('twitter');
 require('dotenv').config();
+
 const twitterClient = new twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
   access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
+
 const baseUrl = 'http://www.repostered.com';
 
 const getSeriesUrl = async () => {
